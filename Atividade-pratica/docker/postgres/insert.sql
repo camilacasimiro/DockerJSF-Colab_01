a7a963f11cc04eab72e4bb8b232c145e42653cc4
camilacasimiro <camilacasimiiro@gmail.com>
2022-03-05 17:48:45 -0300

    Adicionando conexões com o banco

---
+++ Atividade-pratica/docker/postgres/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO integrante (nome, dataDeNascimento, cpf) VALUES ('Mariana',1997-05-20,'1234564');
+INSERT INTO integrante (nome, dataDeNascimento, cpf) VALUES ('Josefa',1989-08-10,'1234458');
+
+INSERT INTO banda (localDeOrigem, nomeFantasia) VALUES ('Pernambuco', 'Asa Branca');
+INSERT INTO banda (localDeOrigem, nomeFantasia) VALUES ('São Paulo', 'Banda Djavu');
+
+INSERT INTO integrante_banda ( id_banda, id_integrante) VALUES (1, 1);
+INSERT INTO integrante_banda ( id_banda, id_integrante) VALUES (2, 2);
